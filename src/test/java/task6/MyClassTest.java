caac01800838c0077801eab3c96cf7f0c4e2ac7c
Evgenyi_Malyutin <evgen986@mail.ru>
2023-09-25 22:23:32 +0300

    [initial commit

---
+++ src/test/java/task6/MyClassTest.java
@@ -0,0 +1,21 @@
+package task6;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyClassTest {
+    @Test
+    public void sumListShouldBeCorrect(){
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3));
+        List<Integer> result = new ArrayList<>(Arrays.asList(2,4,6));
+        List <Integer> resultTest =  MyClass.sumLists(list1, list2);
+        assertEquals(result, resultTest, "Сумма списков не верна!");
+    }
+
+    @Test
+    public void
+}
