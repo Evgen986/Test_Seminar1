534bd812914a1009b50453aeb6eccded7443aecd
Evgenyi_Malyutin <evgen986@mail.ru>
2023-09-26 13:06:00 +0300

    added HW Seminar 1

--- src/test/java/task6/MyClassTest.java
+++ src/test/java/task6/MyClassTest.java
@@ -1,5 +1,6 @@
 package task6;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,15 +8,34 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyClassTest {
+    private static List<Integer> listNumbers1;
+    private static List<Integer> listNumbers2;
+    private static List<String> listWords;
+    @BeforeAll
+    public static void createLists(){
+        listNumbers1 = new ArrayList<>(Arrays.asList(1,2,3));
+        listNumbers2 = new ArrayList<>(Arrays.asList(1,2,3));
+        listWords = new ArrayList<>(Arrays.asList("Hello", "World", "One", "Two"));
+    }
+
     @Test
     public void sumListShouldBeCorrect(){
-        List<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3));
-        List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3));
         List<Integer> result = new ArrayList<>(Arrays.asList(2,4,6));
-        List <Integer> resultTest =  MyClass.sumLists(list1, list2);
+        List <Integer> resultTest =  MyClass.sumLists(listNumbers1, listNumbers2);
         assertEquals(result, resultTest, "Сумма списков не верна!");
     }
 
     @Test
-    public void
+    public void findMaxShouldFindMaxElement(){
+        int result = 3;
+        int resultTest = MyClass.findMax(listNumbers1);
+        assertEquals(result, resultTest, "Метод не находит большее число");
+    }
+
+    @Test
+    public void filterStringShouldFilterListCorrect(){
+        List<String> result = new ArrayList<>(Arrays.asList("Hello", "World"));
+        List<String> resultTest = MyClass.filterStrings(listWords, 4);
+        assertEquals(result, resultTest, "Метод не фильтрует лист слов по заданному условию");
+    }
 }
